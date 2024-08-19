9c7c3774c158957f5924217e49ace252ca647e15
SkShoheb33 <shaikshoheb@gmail.com>
2024-08-19 22:01:25 +0530

    Basic card component

--- healthkard/src/App.js
+++ healthkard/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Button from './app/components/Button'
+import Card from './app/components/Card'
 
 function App() {
   return (
     <div>
       <Button/>
+      <Card/>
     </div>
   )
 }
